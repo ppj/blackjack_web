076530ecf88572b4fd820b7da0603e7ef3fb2e2e
ppj <prasanna_3003@yahoo.com>
2014-08-05 11:34:55 +1000

    added more controls

--- main.rb
+++ main.rb
@@ -45,8 +45,9 @@ end
 
 
 post '/set_player_profile' do
+  session[:alert] = ""
   session[:player_name]  = params[:player_name]
-  session[:player_chips] = params[:player_chips]
+  session[:player_chips] = params[:player_chips].to_i
   redirect :pre_round
 end
 
@@ -57,10 +58,11 @@ end
 
 
 post '/begin_round' do
-  session[:player_bet] = params[:player_bet]
+  session[:player_bet] = params[:player_bet].to_i
   session[:deck] = initialize_deck
   session[:player_cards] = []
   session[:dealer_cards] = []
+  session[:player_stays] = false
   2.times {
     session[:player_cards] << session[:deck].pop
     session[:dealer_cards] << session[:deck].pop
@@ -69,49 +71,64 @@ post '/begin_round' do
 end
 
 
-post '/game/player/hit' do
-  if total(session[:player_cards]) < 21
-    session[:player_cards] << session[:deck].pop
-    redirect :game
-  end
-
-  if total(session[:player_cards]) > 21
-    session[:alert] = "#{session[:player_name]} busted!"
-    redirect :game_over
-  elsif total(session[:player_cards]) == 21
-    if total(session[:dealer_cards]) == 21
-      session[:alert] = "Game pushes"
-    else
-      session[:alert] = "#{session[:player_name]} hit a BlackJack!}"
-    end
-    redirect :game_over
-  end
-end
-
-
-post '/game/player/stay' do
-  while total(session[:dealer_cards]) < 18
-    session[:dealer_cards] << session[:deck].pop
-  end
-  if total(session[:dealer_cards]) <= 21
-    redirect :game
-  end
-
-  if total(session[:dealer_cards]) > 21
-    session[:alert] = "Dealer busted! #{session[:player_name]} wins!"
-    redirect :game_over
-  end
-end
-
-
 get '/game' do
+  unless  session[:player_stays]
+    if total(session[:player_cards]) == 21
+      if total(session[:dealer_cards]) == 21
+        @error = "Both #{session[:player_name]} and Dealer hit BlackJack! Game pushes"
+      else
+        @error = "#{session[:player_name]} hit a BlackJack!"
+        session[:player_chips] += session[:player_bet]
+      end
+    elsif total(session[:player_cards]) > 21
+      @error = "#{session[:player_name]} busted!"
+      session[:player_chips] -= session[:player_bet]
+    end
+  else
+    if total(session[:dealer_cards]) == 21
+      @error = "Dealer hit a BlackJack!"
+      session[:player_chips] -= session[:player_bet]
+    elsif total(session[:dealer_cards]) > 21
+      @error = "Dealer busted @ #{total(session[:dealer_cards])}! #{session[:player_name]} wins!"
+      session[:player_chips] += session[:player_bet]
+    elsif total(session[:player_cards]) == total(session[:dealer_cards])
+      @error = "#{session[:player_name]} and Dealer stay @ #{total(session[:player_cards])}! Game pushes"
+    elsif total(session[:player_cards]) > total(session[:dealer_cards])
+      @error = "#{session[:player_name]} wins. Dealer stays @ #{total(session[:dealer_cards])}"
+      session[:player_chips] += session[:player_bet]
+    else
+      @error = "Dealer wins @ #{total(session[:dealer_cards])}"
+      session[:player_chips] -= session[:player_bet]
+    end
+  end
+
   erb :game
 end
 
 
-get '/game_over' do
-  @error = session[:alert]
-  erb :game_over
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+  redirect :game
 end
 
 
+post '/game/player/stay' do
+  session[:player_stays] = true
+  while total(session[:dealer_cards]) < 18
+    session[:dealer_cards] << session[:deck].pop
+  end
+
+  redirect :game
+
+end
+
+
+post '/play_again' do
+  redirect :pre_round
+end
+
+
+get '/game_over' do
+  #erb :game_over
+  "<h4>Encash your #{session[:player_chips]} chips @ the cash counter, #{session[:player_name]}!</h4>"
+end
