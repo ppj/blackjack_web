91b88b53e3e7ab85cf0b77adcd2de5ab52e35833
ppj <prasanna_3003@yahoo.com>
2014-08-16 00:17:33 +1000

    continuous binding and used .replaceWith() instead of .html()

--- public/application.js
+++ public/application.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
-  $('#hit_form input').click(function() {
+  $(document).on('click', '#hit_form input', function() {
     $.ajax({
       type: 'POST',
       url: '/round/player/hit'
     }).done(function(msg) {
-      $('#game_area').html(msg);
+      $('#game_area').replaceWith(msg);
     });
     // prevent from further execution of the route
     return false; // or event.preventDefault();
