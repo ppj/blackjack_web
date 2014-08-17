6be790588a291b8cac9d7ddcfb3eaebec2e20df0
ppj <prasanna_3003@yahoo.com>
2014-08-17 23:50:47 +1000

    moved ajax actions to individual functions

--- public/application.js
+++ public/application.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+  player_hits();
+  player_stays();
+  dealer_hits();
+
+});
+
+function player_hits() {
   $(document).on('click', '#player_hit_form input', function() {
     $.ajax({
       type: 'POST',
@@ -10,7 +17,9 @@ $(document).ready(function() {
     // prevent from further execution of the route
     return false; // or event.preventDefault();
   });
+}
 
+function player_stays() {
   $(document).on('click', '#player_stay_form input', function() {
     $.ajax({
       type: 'POST',
@@ -21,7 +30,9 @@ $(document).ready(function() {
     // prevent from further execution of the route
     return false; // or event.preventDefault();
   });
+}
 
+function dealer_hits() {
   $(document).on('click', '#dealer_hit_form input', function() {
     $.ajax({
       type: 'POST',
@@ -32,7 +43,4 @@ $(document).ready(function() {
     // prevent from further execution of the route
     return false; // or event.preventDefault();
   });
-
-
-
-});
+}
